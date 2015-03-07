255ea3f86e0f8013da20df136cf50f999a240f97
Andrew Shaffer <darawk@gmail.com>
2015-03-07 12:37:29 -0800

    use immodel-<name> package name format

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ module.exports = function(model, replace) {
   
   // Write it all out so that it works with browserify
   model = model
-    .use(replace['getter-setter'] || require('getter-setter'))
-    .use(replace.cast || require('cast'))
-    .use(replace.methods || require('methods'))
-    .use(replace.attrs || require('attrs'))
-    .use(replace.validation || require('validation'))
-    .use(replace.required || require('required'))
-    .use(replace.defaults || require('defaults'))
-    .use(replace.discriminators || require('discriminators'));
+    .use(replace['getter-setter'] || require('immodel-getter-setter'))
+    .use(replace.cast || require('immodel-cast'))
+    .use(replace.methods || require('immodel-methods'))
+    .use(replace.attrs || require('immodel-attrs'))
+    .use(replace.validation || require('immodel-validation'))
+    .use(replace.required || require('immodel-required'))
+    .use(replace.defaults || require('immodel-defaults'))
+    .use(replace.discriminators || require('immodel-discriminators'));
   
-  (replace.primitives || require('primitives'))(model);
+  (replace.primitives || require('immodel-primitives'))(model);
   return model;
 };
